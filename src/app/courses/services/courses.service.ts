b5552060c8eff6a8779eabf0cbb7331979e5b3f1
wrafanunes <wrsnunes@gmail.com>
2022-05-13 22:12:24 -0300

    chamada HTTP Get e rxjs

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { first, tap } from 'rxjs/operators';
 
 import { Course } from '../models/course';
 
@@ -8,12 +9,15 @@ import { Course } from '../models/course';
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json'
+
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      { _id: '1', name: 'Angular', category: 'Front-End' },
-      { _id: '2', name: '.NET', category: 'Back-End' }
-    ]
+  list() {
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      tap(courses => console.log(courses)
+      )
+    )
   }
 }
